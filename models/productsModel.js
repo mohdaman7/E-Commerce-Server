c5bb3255c06c5da918a6205488bbd41b460fdf6a
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-11-11 16:01:03 +0530

    product model

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,5 +1,33 @@
 import mongoose from "mongoose";
 
 const productsSchema = new mongoose.Schema({
-    
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    image:{
+        type:String
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    quantity:{
+
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    }
 })
+
+const Products = mongoose.model("Products",productsSchema);
+export default Products;
