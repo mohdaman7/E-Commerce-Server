64fe7572a73e6cbedbf2dd52a2050cdf743ba1b6
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-11-11 15:00:24 +0530

    user schema

---
+++ models/productsModel.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+const productsSchema = new mongoose.Schema({
+    
+})
---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required:true,
+    },
+    email: {
+        type: String,
+        required:true
+    },
+    accountCreateDate: {
+        type: Date,
+        default: Date.now,
+        required:true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+    cart: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Cart"
+    }],
+    wishlist: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Wishlist"
+    }],
+    orders: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Orders"
+    }]
+},{timestamps: true}) // Automatically adds createdAt and updatedAt fields
+
+const User = mongoose.model("User",userSchema);
+export default User;
