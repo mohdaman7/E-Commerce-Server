3d59919d6914319ebf30ad4563fb5c2fddf167ed
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-11-12 11:08:01 +0530

    user middleware

---
+++ middlewares/userMiddlewares.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const userToken = (req,res,next) => {
+    try{
+        const token = req.headers["authorization"];
+        if(!token){
+            return res.status(403).json({message: "Token not provided"});
+        }
+        jwt.verify(token,process.env.USER_JWT,(err,decode)=>{
+            if(error){
+                res.status(401).json({message:"unauthorized"})
+            }
+            req.email = decode.email;
+            next();
+        })
+    }catch(error){
+        res.status(500).json({message: "Server error"});
+        next(error);
+    }
+}
